a41ab8989a9cfa7b5e0e2257fa3d953786c3ea7d
Kalashpatel <kalashpatel606@gmail.com>
2025-08-04 12:06:54 +0530

    calculator

---
+++ basic-calculator.js
@@ -0,0 +1,6 @@
+var a = 70, b = 60;
+
+console.log("Addition of both numbers is:", a + b);
+console.log("Substraction of both numbers is:", a - b);
+console.log("Multiplication of both numbers is:", a * b);
+console.log("Division of both numbers is:", a / b);
