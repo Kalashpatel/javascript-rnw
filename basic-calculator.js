3ff2dd5c31e86924a3af147987f5b63ec5a9c651
Kalashpatel <kalashpatel606@gmail.com>
2025-08-04 19:18:15 +0530

    calculator

--- basic-calculator.js
+++ basic-calculator.js
@@ -1,10 +1,10 @@
 var a = 70, b = 60;
 
-console.log("First numbers is:", a);
-console.log("Second numbers is:", b, "\n");
+console.log(`First numbers is: ${a}`);
+console.log(`Second numbers is: ${b} \n`);
 console.log(".......Basic calculation of both numbers.......\n");
-console.log("Addition of both numbers is:", a + b);
-console.log("Substraction of both numbers is:", a - b);
-console.log("Multiplication of both numbers is:", a * b);
-console.log("Division of both numbers is:", a / b);
-console.log("Modulas of both numbers is:", a % b);
+console.log(`Addition of both numbers is: ${a + b}`);
+console.log(`Substraction of both numbers is: ${a - b}`);
+console.log(`Multiplication of both numbers is: ${a * b}`);
+console.log(`Division of both numbers is: ${a / b}`);
+console.log(`Modulas of both numbers is: ${a % b}`);
